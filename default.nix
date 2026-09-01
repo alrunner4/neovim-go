65c8964b70a04bda0903baf6b7f6751442506933
Alex Carter <alex.carter@cinnamon.health>
2026-09-01 13:45:29 -0700

    trying to get golangci-lint to work in a monmorepo

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ BASH   = "${pkgs.bash}/bin/bash";
 CHMOD  = "${pkgs.coreutils}/bin/chmod";
 CP     = "${pkgs.coreutils}/bin/cp";
 GOPLS  = "${pkgs.gopls}/bin/gopls";
+LINT   = "${pkgs.golangci-lint}/bin/golangci-lint";
 LN     = "${pkgs.coreutils}/bin/ln";
 MKDIR  = "${pkgs.coreutils}/bin/mkdir";
 NEOVIM = "${pkgs.neovim}/bin/nvim";
@@ -23,12 +24,36 @@ in derivation {
     builder = pkgs.writeShellScript "configure-neovim-go" ''
         ${MKDIR} -p $out/etc/lsp
         ${CP} ${lspconfig-gopls} $out/etc/lsp/gopls.lua
+
         ${MKDIR} -p $out/etc/lsp/gopls
         VERSION=$(${GOPLS} version)
-        printf "\
-        vim.print('Enabling Go Language Server: $VERSION')
+        printf >$out/etc/lsp/gopls/enable.lua "\
+        vim.print('Enabling Go Language Server: $(${GOPLS} version), $(${LINT} version)')
+        vim.lsp.config('golangci_lint', {
+            cmd = { 'golangci-lint-langserver' },
+            filetypes = { 'go', 'gomod' },
+            root_markers = { '.golangci.yaml' },
+            init_options = {
+                command = {
+                    'golangci-lint',
+                    'run',
+                    '--output.json.path=stdout',
+                    '--show-stats=false',
+                }
+            },
+            handlers = {
+                ["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
+                    if result and result.uri then
+                        vim.print("golangci-lint URI: " .. result.uri)
+                    end
+                    vim.lsp.handlers["textDocument/publishDiagnostics"](err, result, ctx, config)
+                end
+            }
+        })
         vim.lsp.enable('gopls')
-        " > $out/etc/lsp/gopls/enable.lua
+        vim.lsp.enable('golangci_lint')
+        "
+
         printf "\
         anoremenu PopUp.-LSP- <NOP>
         anoremenu PopUp.Inspect      :lua vim.lsp.buf.hover()<CR>
@@ -37,10 +62,11 @@ in derivation {
         nnoremap gD :lua vim.lsp.buf.definition({reuse_win = true})<CR>
         nnoremap <C-Space> :lua vim.lsp.buf.code_action()<CR>
         " > $out/etc/lsp/gopls/enable.vim
+
         ${MKDIR} -p $out/bin
         printf "\
         #!${BASH}
-        export PATH=\$PATH:${pkgs.go}/bin:${pkgs.gopls}/bin
+        export PATH=\$PATH:${pkgs.go}/bin:${pkgs.gopls}/bin:${pkgs.golangci-lint}/bin:${pkgs.golangci-lint-langserver}/bin
         exec ${NEOVIM} \
             --cmd \"set runtimepath+=$out/etc\" \
             --cmd \"runtime! lsp/gopls/enable.lua lsp/gopls/enable.vim\" \
